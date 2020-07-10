75341e539ed38521b3586e1db99abc3d99fa5705
Brad Rivenburgh <brad.rivenburgh@gmail.com>
2020-07-10 11:16:13 -0400

    added accordion menu with state and props; imported enzyme into project to test state

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Counter from './state/Counter';
 import HelloWorld from './state-drills/HelloWorld';
 import Bomb from './state-drills/Bomb';
 import RouletteGun from './state-drills/RouletteGun';
+import Tabs from './state/Tabs';
 
 // make 2 tooltips here and another inside the App directly
 const firstTooltip = (
@@ -22,10 +23,19 @@ const secondTooltip = (
   </Tooltip>
 );
 
+const tabsProp = [
+  { name: 'First tab',
+    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam exercitationem quos consectetur expedita consequatur. Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque.' },
+  { name: 'Second tab',
+    content: 'Laboriosam exercitationem quos consectetur expedita consequatur. Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque. Lorem ipsum dolor sit amet consectetur adipisicing elit.' },
+  { name: 'Third tab',
+    content: 'Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque. Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam exercitationem quos consectetur expedita consequatur.' },
+];
+
 function App() {
   return (
     <main className='App'>
-      <Split className='left' flexBasis='2'>
+      <Split className='left' flexBasis='1'>
         This is the content for the left Split. Lorem {firstTooltip} dolor sit amet consectetur, adipisicing elit. Incidunt ex velit suscipit facere officia?<br />
         {/* make another tooltip directly inside the App */}
         <Tooltip message='one more tooltip message'>
@@ -34,7 +44,9 @@ function App() {
         <Messages name="Messages" unread={0}/>
         <Messages name="Notifications" unread={10}/>
         <RouletteGun bulletInChamber={8}/>
+        <Tabs tabs={tabsProp} />
       </Split>
+      
       <Split className='right'>
         This is the content for the right Split. Inventore aliquid cupiditate suscipit repellat. Quaerat quis {secondTooltip} quam fuga. Aliquid quo possimus id soluta aspernatur.
       <TheDate />
